adf9cdf8706301c253c1e107d9d973e29390b41f
Hosein Balforoosh <hbalforoosh@gmail.com>
2025-07-27 17:38:40 +0330

    new change To complete the exercise based on returning the sequence and not the value

--- CW9_10.py
+++ CW9_10.py
@@ -13,8 +13,11 @@ def time_decorator(func):
 
 
 @time_decorator
-def test(n):
-    return n * 2
+def fibo_list(n):
+    result = []
+    for i in range(n + 1):
+        result.append(fibo(i))
+    return result
 
 
 def fibo(n):
@@ -23,46 +26,4 @@ def fibo(n):
     return fibo(n - 1) + fibo(n - 2)
 
 
-# def gen_fibo(n):
-#     for i in range(n):
-#         yield fibo(i)
-
-
-# for j in gen_fibo(5):
-#     print(j, end=",")
-
-
-@time_decorator
-def fibo_generator(n):
-    a, b = 0, 1
-    for i in range(n):
-        yield a
-        a, b = b, a + b
-
-
-for item in fibo_generator(10):
-    print(item)
-
-
-print(list(fibo_generator(10)))
-
-import time
-
-
-def time_decorator(func):
-    def wrapper(n):
-        start_time = time.time()
-        result = func(n)
-        end_time = time.time()
-        print(f"duration = {end_time - start_time}")
-        return result
-
-    return wrapper
-
-
-@time_decorator
-def test(n):
-    return n * 2
-
-
-print(test(10))
+print(fibo_list(5))
